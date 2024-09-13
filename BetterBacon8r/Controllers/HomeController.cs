382c973255d13f9beca72dd5d76649b5d9dec1bc
al <almail>
2024-09-13 16:16:56 -0400

    added favicon and code

--- BetterBacon8r/Controllers/HomeController.cs
+++ BetterBacon8r/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using BetterBacon8r.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Web;
 
 namespace BetterBacon8r.Controllers {
 
@@ -10,7 +13,11 @@ namespace BetterBacon8r.Controllers {
         private readonly ILogger<HomeController> _logger;
         private readonly HttpClient _wikiClient;
 
-        public HomeController(
+        [RegularExpression("<a[^>]*href=[\"']/wiki/[^\"']*[\"'][^>]*>(.*?)<\\/a>")]
+        string pattern = @"<a[^>]*href=[""']/wiki/[^""']*[""'][^>]*>(.*?)<\/a>";
+
+
+    public HomeController(
             IWebHostEnvironment env,
             ILogger<HomeController> logger,
             IHttpClientFactory factory
@@ -21,16 +28,57 @@ namespace BetterBacon8r.Controllers {
         }
 
         public async Task<IActionResult> Index() {
+            var seedWords = await GetNewWords();
+                
+            return View(seedWords);
+        }
+
+        public async Task<IEnumerable<string>> GetNewWords() {
             string webRootPath = _env.WebRootPath;
             string filePath = Path.Combine(webRootPath, "seedWords.json");
             string jsonContent = await System.IO.File.ReadAllTextAsync(filePath);
             List<string> seedWords = JsonSerializer.Deserialize<List<string>>(jsonContent)!;
 
-            return View(seedWords);
+            return GetTruncatedAndRandomizedWords(seedWords);
         }
 
-        public IActionResult Privacy() {
-            return View();
+        private static IEnumerable<string> GetTruncatedAndRandomizedWords(List<string> seedWords) {
+            return seedWords
+                .Distinct()
+                .OrderBy(x => Guid.NewGuid())
+                .Take(250)
+                .OrderBy(x => x)
+                .ToList();
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetNextWords([FromQuery]string word) {
+            var result = await (await _wikiClient.GetAsync(FormatWord(word))).Content.ReadAsStringAsync();
+
+            MatchCollection matches = Regex.Matches(result, pattern);
+            var toReturn = new List<string>();
+
+            foreach (Match match in matches) {
+                toReturn.Add(match.Groups[1].Value);
+            }
+
+            List<string> FilterLinkText(List<string> wordsToFilter) {
+                return wordsToFilter
+                    .Where(e => 
+                        e.Contains("<", StringComparison.OrdinalIgnoreCase) is false && 
+                        e.Contains("span", StringComparison.OrdinalIgnoreCase) is false &&
+                        e.Contains("CS1", StringComparison.OrdinalIgnoreCase) is false &&
+                        e.Contains("wikipedia", StringComparison.OrdinalIgnoreCase) is false &&
+                        e.Equals(word, StringComparison.OrdinalIgnoreCase) is false &&
+                        e.Contains("&#", StringComparison.OrdinalIgnoreCase) is false)
+                    .ToList();
+            }
+
+            string FormatWord(string word) {
+                return word.Replace(" ", "_");
+            }
+
+            return GetTruncatedAndRandomizedWords(FilterLinkText(toReturn));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
